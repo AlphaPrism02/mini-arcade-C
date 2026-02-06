2b9aa9a3adeb80124bb025239a431b0da17eaef3
gauthamsunil76941 <gauthamsunil0007@gmail.com>
2026-02-06 17:34:49 +0000

    number guessing added

--- main.c
+++ main.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 #include </workspaces/mini-arcade-C/tictactoe.c>
 
 void main()
 {
     //add menu to start different games
-    start_tictactoe();
+    char name[50];
+    int choice;
+    printf("ENTER NAME: \n");
+    fgets(name, sizeof(name), stdin);  
+    printf(".......MINI ARCADE GAMES.......\n");
+    Sleep(1000);
+    printf("HEYYYY %s \n", name);
+    Sleep(800);
+
+    printf("Feeling brave enough for TIC TAC TOE?\n");
+    printf("1 - Bring it on \n");
+    printf("2 - I'll warm up with mini games \n");
+    scanf("%d",&choice);
+
+    switch(choice){
+        case 1:
+        start_tictactoe();
+        break;
+        case 2:
+        
+    }
+    
+}
+
+int number_guessing(){
+    srand(time(NULL));
+    int guess,tries=0;
+    int min=1;
+    int max=100;
+    int ans=(rand()%(max-min+1))+min;
+    do{
+        printf("GUESS A NUMBER BETWEEN 0 - 100\n");
+        scanf("%d",&guess)
+        tries++;
+    }while(guess!=ans);
+    printf("THE ANSWER IS %d\n",ans);
+    printf("THE TOOK YOU %d TIMES\n",tries);
+    return 0;
 }
