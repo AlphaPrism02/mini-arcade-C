e196685ccb248cad9b7ef5bc85e5823925a21c11
somyakhare16 <khare.somya16@gmail.com>
2026-02-18 18:41:40 +0000

    added connect 4

--- 4connect.c
+++ 4connect.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ROWS 6
 #define COLS 7
@@ -7,15 +9,15 @@ char board[ROWS][COLS];
 
 // Initialize board
 void initBoard() {
-    for(int i=0;i<ROWS;i++)
-        for(int j=0;j<COLS;j++)
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
             board[i][j] = '.';
 }
 
 // Print board
 void printBoard() {
-    for(int i=0;i<ROWS;i++) {
-        for(int j=0;j<COLS;j++)
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++)
             printf("%c ", board[i][j]);
         printf("\n");
     }
@@ -23,9 +25,9 @@ void printBoard() {
 }
 
 // Drop piece
-int drop(int col, char player) {
-    for(int i=ROWS-1;i>=0;i--) {
-        if(board[i][col] == '.') {
+int dropPiece(int col, char player) {
+    for (int i = ROWS - 1; i >= 0; i--) {
+        if (board[i][col] == '.') {
             board[i][col] = player;
             return 1;
         }
@@ -33,51 +35,92 @@ int drop(int col, char player) {
     return 0;
 }
 
-// Check horizontal win only (simple version)
+// Check win
 int checkWin(char p) {
-    for(int i=0;i<ROWS;i++) {
-        for(int j=0;j<COLS-3;j++) {
-            if(board[i][j]==p &&
-               board[i][j+1]==p &&
-               board[i][j+2]==p &&
-               board[i][j+3]==p)
+
+    // Horizontal
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS - 3; j++)
+            if (board[i][j]==p && board[i][j+1]==p &&
+                board[i][j+2]==p && board[i][j+3]==p)
                 return 1;
-        }
-    }
+
+    // Vertical
+    for (int i = 0; i < ROWS - 3; i++)
+        for (int j = 0; j < COLS; j++)
+            if (board[i][j]==p && board[i+1][j]==p &&
+                board[i+2][j]==p && board[i+3][j]==p)
+                return 1;
+
+    // Diagonal \
+    for (int i = 0; i < ROWS - 3; i++)
+        for (int j = 0; j < COLS - 3; j++)
+            if (board[i][j]==p && board[i+1][j+1]==p &&
+                board[i+2][j+2]==p && board[i+3][j+3]==p)
+                return 1;
+
+    // Diagonal /
+    for (int i = 3; i < ROWS; i++)
+        for (int j = 0; j < COLS - 3; j++)
+            if (board[i][j]==p && board[i-1][j+1]==p &&
+                board[i-2][j+2]==p && board[i-3][j+3]==p)
+                return 1;
+
     return 0;
+}
+
+// Computer move (random column)
+int computerMove() {
+    int col;
+    do {
+        col = rand() % COLS;
+    } while (!dropPiece(col, 'O'));
+    return col;
 }
 
 int main() {
     int col, turn = 0;
-    char player;
 
+    srand(time(0));
     initBoard();
 
-    while(1) {
+    while (1) {
         printBoard();
 
-        player = (turn % 2 == 0) ? 'X' : 'O';
+        if (turn % 2 == 0) {
+            // Player move
+            printf("Your turn (X). Choose column (1-7): ");
+            scanf("%d", &col);
+            col--;
 
-        printf("Player %c choose column (1-7): ", player);
-        scanf("%d", &col);
+            if (col < 0 || col >= COLS || !dropPiece(col, 'X')) {
+                printf("Invalid move!\n");
+                continue;
+            }
 
-        col--; // index adjust
+            if (checkWin('X')) {
+                printBoard();
+                printf("You win!\n");
+                break;
+            }
 
-        if(col < 0 || col >= COLS || !drop(col, player)) {
-            printf("Invalid move!\n");
-            continue;
-        }
+        } else {
+            // Computer move
+            int c = computerMove();
+            printf("Computer chose column %d\n", c + 1);
 
-        if(checkWin(player)) {
-            printBoard();
-            printf("Player %c wins!\n", player);
-            break;
+            if (checkWin('O')) {
+                printBoard();
+                printf("Computer wins!\n");
+                break;
+            }
         }
 
         turn++;
 
-        if(turn == ROWS*COLS) {
-            printf("Draw!\n");
+        if (turn == ROWS * COLS) {
+            printBoard();
+            printf("Game draw!\n");
             break;
         }
     }
